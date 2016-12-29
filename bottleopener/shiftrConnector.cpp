f2ac1a29b2a3317042bfce6ce569a48004194317
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-12-29 13:31:36 +0100

    First commit of Shitr.io connector

---
+++ bottleopener/shiftrConnector.cpp
@@ -0,0 +1,43 @@
+#include "shiftrConnector.h"
+
+#include <Bridge.h>
+
+void ShiftrConnector::init(const char* deviceLogin, const char* pwd)
+{
+  client.begin("broker.shiftr.io");
+
+  this->deviceLogin = deviceLogin;
+  this->pwd = pwd;
+
+  connect();
+
+  client.subscribe("/bottle-openner");
+  // client.unsubscribe("/bottle-openner");
+
+}
+
+void ShiftrConnector::connect() {
+  while (!client.connect("arduino", this->deviceLogin, this->pwd)) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\nconnected to Shiftr!");
+}
+
+void ShiftrConnector::loop()
+{
+  client.loop();
+
+  if (!client.connected()) {
+    connect();
+  }
+}
+
+void ShiftrConnector::sendCounter(int counter)
+{
+  char buf[5];
+  itoa(counter, buf, 10);
+  
+  client.publish("/bottle-openner", buf);
+}
