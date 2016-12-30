bfa4e100fb2906d0de9c97d5eb8688828cd96458
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-12-30 15:56:45 +0100

    New simple Log class

---
+++ counter/logger.cpp
@@ -0,0 +1,19 @@
+#include "logger.h"
+
+Logger *logger = new Logger();
+
+void Logger::init()
+{
+#ifdef LOG_TO_CONSOLE
+  Serial.begin(9600);
+  while (!Serial);
+#endif
+}
+
+void Logger::log(String text)
+{
+#ifdef LOG_TO_CONSOLE
+  Serial.print(text);
+#endif
+}
+
---
+++ counter/logger.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Arduino.h>
+
+//comment following line to disable Console log and be able to run application without it
+#define LOG_TO_CONSOLE  1
+
+class Logger {
+  public :
+    
+    void init();
+    void log(String text);
+
+    Logger() {};
+};
+
+extern Logger *logger;
