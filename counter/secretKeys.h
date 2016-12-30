87d6838386ea92c896dfbaa24d9f66d37537099a
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-12-30 15:57:58 +0100

    New secretKeys.h file to store all API keys

---
+++ counter/secretKeys.h
@@ -0,0 +1,11 @@
+#pragma
+
+//ThingSpeak API keys
+#define THINGSPEAK_CHANNEL_NUMBER   204611
+#define THINGSPEAK_WRITE_API_KEY    "905T91H5LO1KQIG3"
+#define THINGSPEAK_READ_API_KEY     "8V00S9EQCORGL5BU"
+
+//Shiftr.io tokens
+#define SHIFTR_NAMESPACE      "/bottle-openner"
+#define SHIFTR_DEVICE_LOGIN   "counteryun1"
+#define SHIFTR_DEVICE_PWD     "c84fafe9d3fc89f3"
