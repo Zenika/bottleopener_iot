e5c81b68122456710b736a92ad3d4c80f7819d9b
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-01-01 19:41:43 +0100

    add of secretKeys example file

---
+++ server/src/secretkeys.js.example.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gwennael.buchet on 01/01/17.
+ */
+
+/**
+ * HOW-TO :
+ * - rename this file into "secretKeys.js"
+ * - replace values with your own
+ */
+
+//ThingSpeak API keys
+const THINGSPEAK_CHANNEL_NUMBER = 0;
+const THINGSPEAK_WRITE_API_KEY = "";
+const THINGSPEAK_READ_API_KEY = "";
+
+//Shiftr.io tokens
+const SHIFTR_NAMESPACE = "/xxx";
+const SHIFTR_DEVICE_LOGIN = "";
+const SHIFTR_DEVICE_PWD = "";
