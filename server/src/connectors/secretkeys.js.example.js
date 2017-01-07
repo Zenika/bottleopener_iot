2fca6a859ef8a3ac1a2b7729839a3a0ab2c7a4e2
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-01-07 17:09:41 +0100

    Use module system for secret keys

--- server/src/connectors/secretkeys.js.example.js
+++ server/src/connectors/secretkeys.js.example.js
@@ -9,12 +9,16 @@
  * - replace values with your own
  */
 
-//ThingSpeak API keys
-const THINGSPEAK_CHANNEL_NUMBER = 0;
-const THINGSPEAK_WRITE_API_KEY = "";
-const THINGSPEAK_READ_API_KEY = "";
+module.exports = Object.freeze(
+	{
+		//ThingSpeak API keys
+		THINGSPEAK_CHANNEL_NUMBER: 0,
+		THINGSPEAK_WRITE_API_KEY: "",
+		THINGSPEAK_READ_API_KEY: "",
 
-//Shiftr.io tokens
-const SHIFTR_NAMESPACE = "/xxx";
-const SHIFTR_DEVICE_LOGIN = "";
-const SHIFTR_DEVICE_PWD = "";
+		//Shiftr.io tokens
+		SHIFTR_NAMESPACE: "/xxxx",
+		SHIFTR_DEVICE_LOGIN: "",
+		SHIFTR_DEVICE_PWD: ""
+	}
+);
