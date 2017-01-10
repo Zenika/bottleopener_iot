21f2a73c69fe6f6ad7d06b613cb15b9a5f2fefde
Adrien Legrand <adrien.legrand@zenika.com>
2017-01-10 16:46:09 +0100

    AWS IoT connector working

--- bottleopener_iot_ino_backup/bottleopener_iot_ino/aws_iot_config.h
+++ bottleopener_iot_ino_backup/bottleopener_iot_ino/aws_iot_config.h
@@ -1,19 +1,19 @@
-#define AWS_IOT_MQTT_HOST "a13byqleafkxa1.iot.us-west-2.amazonaws.com" 
+#define AWS_IOT_MQTT_HOST "" 
 // your endpoint
 #define AWS_IOT_MQTT_PORT 8883
 // your port (default is 8883)
-#define AWS_IOT_CLIENT_ID "decap"
+#define AWS_IOT_CLIENT_ID ""
 // your client ID
-#define AWS_IOT_MY_THING_NAME "decap"
+#define AWS_IOT_MY_THING_NAME ""
 // your thing name
-#define AWS_IOT_ROOT_CA_FILENAME "root-ca.pem.crt"
+#define AWS_IOT_ROOT_CA_FILENAME ""
 // your root-CA filename
-#define AWS_IOT_CERTIFICATE_FILENAME "d1d0dc3351-certificate.pem.crt"
+#define AWS_IOT_CERTIFICATE_FILENAME ""
 // your certificate filename
-#define AWS_IOT_PRIVATE_KEY_FILENAME "d1d0dc3351-private.pem.key"
+#define AWS_IOT_PRIVATE_KEY_FILENAME ""
 // your private key filename
 //===============================================================
-#define AWS_IOT_PATH_PREFIX "/root/AWS-IoT-Python-Runtime/certs/"
+#define AWS_IOT_PATH_PREFIX "/root/AWS-IoT-Python-Runtime/certs/" //usually this path is used
 #define AWS_IOT_ROOT_CA_PATH AWS_IOT_PATH_PREFIX AWS_IOT_ROOT_CA_FILENAME
 #define AWS_IOT_CERTIFICATE_PATH AWS_IOT_PATH_PREFIX AWS_IOT_CERTIFICATE_FILENAME
 #define AWS_IOT_PRIVATE_KEY_PATH AWS_IOT_PATH_PREFIX AWS_IOT_PRIVATE_KEY_FILENAME
--- bottleopener_iot_ino_backup/bottleopener_iot_ino/bottleopener_iot_ino.ino
+++ bottleopener_iot_ino_backup/bottleopener_iot_ino/bottleopener_iot_ino.ino
@@ -21,55 +21,29 @@ char msg[32]; // read-write buffer
 int cnt = 0; // loop counts
 int rc = -100; // return value placeholder
 bool success_connect = false; // whether it is connected
-int ledPin = 8; // LED connected to digital pin 13
-int inPin = 4;   // pushbutton connected to digital pin 7
+int inPin = 10;   // pushbutton connected to digital pin 7
 int val = LOW;     // variable to store the read value
-
-
-// Basic callback function that prints out the message
-void msg_callback(char* src, unsigned int len, Message_status_t flag) {
-  if (flag == STATUS_NORMAL) {
-    Serial.println("Message received");
-    char dst[len];
-    strcpy(dst, src); /* BANG!!! */
-    strncpy(dst, src, len);
-    Serial.println(dst);
-    int state = 0;
-    Serial.println(state);
-    if (state == 1) {
-      digitalWrite(ledPin, HIGH);
-    }
-    else {
-      digitalWrite(ledPin, LOW);
-    }
-  }
-}
 
 void setup() {
   // Start Serial for print-out and wait until it's ready
   Serial.begin(9600);
   while (!Serial);
+
   //
-  pinMode(ledPin, OUTPUT);
+   Serial.println("Try to connect to AWS IoT ...\n");
   pinMode(inPin, INPUT);
   digitalWrite(inPin, LOW);
-  digitalWrite(ledPin, LOW);
   char curr_version[80];
   snprintf_P(curr_version, 80, PSTR("AWS IoT SDK Version(dev) %d.%d.%d-%s\n"), VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
   Serial.println(curr_version);
+  
   // Set up the client
-  Serial.println(AWS_IOT_MY_THING_NAME);
   if ((rc = myClient.setup("decap")) == 0) {
     // Load user configuration
     if ((rc = myClient.config(AWS_IOT_MQTT_HOST, AWS_IOT_MQTT_PORT, AWS_IOT_ROOT_CA_PATH, AWS_IOT_PRIVATE_KEY_PATH, AWS_IOT_CERTIFICATE_PATH)) == 0) {
       // Use default connect: 60 sec for keepalive
       if ((rc = myClient.connect()) == 0) {
         success_connect = true;
-  
-        if ((rc = myClient.subscribe("ledState", 1, msg_callback)) != 0) {
-          Serial.println("Subscribe failed!");
-          Serial.println(rc);
-        }
       }
       else {
         Serial.println(F("Connect failed!"));
@@ -87,6 +61,7 @@ void setup() {
   }
   // Delay to make sure SUBACK is received, delay time could vary according to the server
   delay(2000);
+  Serial.println("... done \n");
 }
 
 void loop() {
