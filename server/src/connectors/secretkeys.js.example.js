1ee81d229bd685a66c4c0a99d6b270ef4a5a9cf6
Adrien Legrand <adrien.legrand@zenika.com>
2017-01-10 13:04:57 +0100

    secret.js.example.js update for AWS IoT

--- server/src/connectors/secretkeys.js.example.js
+++ server/src/connectors/secretkeys.js.example.js
@@ -19,6 +19,20 @@ module.exports = Object.freeze(
 		//Shiftr.io tokens
 		SHIFTR_NAMESPACE: "/xxxx",
 		SHIFTR_DEVICE_LOGIN: "",
-		SHIFTR_DEVICE_PWD: ""
+		SHIFTR_DEVICE_PWD: "",
+
+		//AWS IoT Cas & config
+        AWS_IOT_MQTT_HOST: "",
+        AWS_IOT_MQTT_PORT: 8883,
+        // your port (default is 8883)
+        AWS_IOT_CLIENT_ID: "",
+        AWS_IOT_MY_THING_NAME: "",
+        AWS_IOT_ROOT_CA_FILENAME: "",
+        AWS_IOT_CERTIFICATE_FILENAME: "",
+        AWS_IOT_PRIVATE_KEY_FILENAME: "",
+        AWS_IOT_PATH_PREFIX: "/path/to/your/certs/",
+        AWS_IOT_ROOT_CA_PATH: AWS_IOT_PATH_PREFIX+AWS_IOT_ROOT_CA_FILENAME,
+        AWS_IOT_CERTIFICATE_PATH: AWS_IOT_PATH_PREFIX+AWS_IOT_CERTIFICATE_FILENAME,
+        AWS_IOT_PRIVATE_KEY_PATH: AWS_IOT_PATH_PREFIX+AWS_IOT_PRIVATE_KEY_FILENAME
 	}
 );
