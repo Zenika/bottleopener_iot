320f1706f67942466996a8e57748e152e83be641
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-01-12 17:01:23 +0100

    Fix Carriots request's header

--- server/src/connectors/carriots.module.js
+++ server/src/connectors/carriots.module.js
@@ -9,12 +9,11 @@ exports.init = function (mainCallback) {
 
 	let options = {
 		host: "api.carriots.com",
-		path: "/streams",
+		path: "/streams/?max=1&order=-1",
+		port: 443,
 		method: "GET",
 		headers: {
 			"Accept": "application/json",
-			"Accept-Encoding": "gzip, deflate, sdch, br",
-			"Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4",
 			"carriots.apiKey": keys.CARRIOTS_APIKEY,
 			"Connection": "keep-alive",
 			"Content-Type": "application/json",
@@ -23,22 +22,22 @@ exports.init = function (mainCallback) {
 	};
 
 	setInterval(function () {
-		let callback = function (response) {
-			let str = '';
-			response.on('data', function (chunk) {
-				str += chunk;
-				mainCallback(chunk);
-			});
+		let req = https.request(options, (res) => {
+			res.on('data', (chunk) => {
+				let body = [];
+				body.push(chunk);
+				let msg = JSON.parse(body.toString());
+				let data = msg.result[0].data;
 
-			response.on('end', function () {
-				console.log(str);
+				mainCallback(data.sender, data.quantity, "Carriots");
 			});
-		};
+		});
 
-		let req = https.request(options, callback);
-		req.on("error", function (e) {
-			console.log("problem with request: " + e.message);
+		req.on('error', (e) => {
+			console.error(e);
 		});
 		req.end();
+
 	}, 5000);
+
 };
